3cfebb7225b47c132d9dc68f7e3862dcf5aa0ce5
nntt1210 <58521730+nntt1210@users.noreply.github.com>
2021-12-02 18:02:50 +0700

    add scripts to generate db and data

---
+++ insert.sql
@@ -0,0 +1,25 @@
+CATEGORIES
+insert into categories values(1, 'Electronics', null);
+insert into categories values(2, 'Furniture', null);
+insert into categories values(3, 'Houseware', null);
+insert into categories values(4, 'Laptop', 1);
+insert into categories values(5, 'Phone', 1);
+insert into categories values(6, 'iPad', 1);
+insert into categories values(7, 'Bed', 2);
+insert into categories values(8, 'Desk', 2);
+insert into categories values(9, 'Cabinet', 2);
+insert into categories values(10, 'Coffee press', 3);
+insert into categories values(11, 'Chopping board', 3);
+insert into categories values(12, 'Baking dish', 3);
+
+USERS
+insert into users values(1, "tmnguyen", "123456", "Tam Nguyen", "2021-10-12", "abc", "tmnguyen@gmail.com", 1);
+
+
+PRODUCTS
+insert into products values(1, 'Laptop Lenovo Ideapad 3 81WH', "https://cdn.cellphones.com.vn/media/catalog/product/cache/7/image/9df78eab33525d08d6e5fb8d27136e95/l/a/laptop-ksp-v2-004.jpg", 4, "500", "450", "abc", "2021-11-30", "2021-12-05", 3, 1);
+insert into products values(2, 'Laptop HP 14-CF2033WM 3V7G4UA', "https://cdn.cellphones.com.vn/media/catalog/product/cache/7/image/9df78eab33525d08d6e5fb8d27136e95/5/c/5cb9252e618c4537be38b0b8ea227d29-2.jpg", 4, "550", "500", "abc", "2021-11-30", "2021-12-03", 3, 1);
+insert into products values(3, 'Laptop ASUS Vivobook R565EA-UH31T', "https://cdn.cellphones.com.vn/media/catalog/product/cache/7/image/9df78eab33525d08d6e5fb8d27136e95/_/0/_0003_rwo2bv-ksp.jpg", 4, "700", "600", "abc", "2021-11-30", "2021-12-03", 3, 1);
+insert into products values(4, 'Laptop Acer Aspire 3 A315-56-502X NX.HS5SV.00F', "https://cdn.cellphones.com.vn/media/catalog/product/cache/7/image/9df78eab33525d08d6e5fb8d27136e95/1/_/1_66_5.jpg", 4, "650", "600", "abc", "2021-11-30", "2021-12-02", 3, 1);
+insert into products values(5, 'Laptop ASUS Gaming ROG Strix G15 G513IH-HN015T', "https://cdn.cellphones.com.vn/media/catalog/product/cache/7/image/9df78eab33525d08d6e5fb8d27136e95/l/a/laptop-asus-rog-strix-g15-g513ih-hn015t-1-jj.jpg", 4, "1000", "900", "abc", "2021-11-30", "2021-12-02", 3, 1);
+insert into products values(6, 'Surface Pro 7 Core i5 / 8GB / 128GB Chính Hãng', "https://cdn.cellphones.com.vn/media/catalog/product/cache/7/image/9df78eab33525d08d6e5fb8d27136e95/s/u/surface-pro-7-2_1-ksp00978v2.jpg", 4, "900", "850", "abc", "2021-11-30", "2021-12-02", 3, 1);
---
+++ online_auction.sql
@@ -0,0 +1,128 @@
+CREATE DATABASE  IF NOT EXISTS `onlineauction` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `onlineauction`;
+
+-- ----------------------------
+-- Table structure for users
+-- ----------------------------
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `userId` int unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(30) NOT NULL,
+  `fullName` varchar(30) NOT NULL,
+  `dob` date NOT NULL,
+  `address` varchar(50) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `permission` tinyint unsigned,
+  PRIMARY KEY (`userId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- ----------------------------
+-- Table structure for categories
+-- ----------------------------
+DROP TABLE IF EXISTS `categories`;
+CREATE TABLE `categories` (
+  `catId` int unsigned NOT NULL AUTO_INCREMENT,
+  `catName` varchar(50) COLLATE utf8_general_ci NOT NULL,
+  `parentId` int unsigned,
+  PRIMARY KEY (`catId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- ----------------------------
+-- Table structure for products
+-- ----------------------------
+DROP TABLE IF EXISTS `products`;
+CREATE TABLE `products` (
+  `proId` int unsigned NOT NULL AUTO_INCREMENT,
+  `proName` varchar(50) NOT NULL,
+  `proImg` varchar(2083) NOT NULL,
+  `catId` int unsigned NOT NULL,
+  `currentPrice` int unsigned NOT NULL,
+  `buyNowPrice` int unsigned DEFAULT NULL,
+  `fullDes` varchar(200) NOT NULL,
+  `postDate` datetime NOT NULL,
+  `expiredDate` datetime NOT NULL,
+  `numberOfBids` tinyint unsigned DEFAULT 0,
+  `bidderId` int unsigned DEFAULT NULL,
+  PRIMARY KEY (`proId`),
+  CONSTRAINT `FK_products_categories` FOREIGN KEY (`catId`) REFERENCES `categories` (`catId`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `FK_products_bidders` FOREIGN KEY (`bidderId`) REFERENCES `users` (`userId`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- ----------------------------
+-- Table structure for watchlist
+-- ----------------------------
+DROP TABLE IF EXISTS `watchlist`;
+CREATE TABLE `watchlist` (
+  `bidderId` int unsigned NOT NULL,
+  `proId` int unsigned NOT NULL,
+  PRIMARY KEY (`bidderId`, `proId`),
+  CONSTRAINT `FK_watchlist_users` FOREIGN KEY (`bidderId`) REFERENCES `users` (`userId`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `FK_watchlist_products` FOREIGN KEY (`proId`) REFERENCES `products` (`proId`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- ----------------------------
+-- Table structure for sessions
+-- ----------------------------
+DROP TABLE IF EXISTS `sessions`;
+CREATE TABLE `sessions` (
+  `sessionId` int unsigned NOT NULL,
+  `expires` int unsigned NOT NULL,
+  `data` varchar(100) NOT NULL,
+  `userId` int unsigned NOT NULL,
+  PRIMARY KEY (`sessionId`),
+  CONSTRAINT `FK_sessions_users` FOREIGN KEY (`userId`) REFERENCES `users` (`userId`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- ----------------------------
+-- Table structure for auction history
+-- ----------------------------
+DROP TABLE IF EXISTS `auctionHistory`;
+CREATE TABLE `auctionHistory` (
+  `proId` int unsigned NOT NULL,
+  `auctionTime` datetime NOT NULL,
+  `bidderId` int unsigned NOT NULL,
+  `auctionPrice` int NOT NULL,
+  PRIMARY KEY (`proId`, `auctionTime`),
+  CONSTRAINT `FK_auctionHistory_products` FOREIGN KEY (`proId`) REFERENCES `products` (`proId`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `FK_auctionHistory_users` FOREIGN KEY (`bidderId`) REFERENCES `users` (`userId`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- ----------------------------
+-- Table structure for rating history
+-- ----------------------------
+DROP TABLE IF EXISTS `ratingHistory`;
+CREATE TABLE `ratingHistory` (
+  `bidderId` int unsigned NOT NULL,
+  `assessorId` int unsigned NOT NULL,
+  `ratingTime` datetime NOT NULL,
+  `ratingPoint` tinyint unsigned NOT NULL,
+  `comment` varchar(100) NOT NULL,
+  PRIMARY KEY (`bidderId`, `assessorId`, `ratingTime`),
+  CONSTRAINT `FK_ratingHistory_users_1` FOREIGN KEY (`bidderId`) REFERENCES `users` (`userId`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `FK_ratingHistory_users_2` FOREIGN KEY (`assessorId`) REFERENCES `users` (`userId`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- ----------------------------
+-- Table structure for auction list
+-- ----------------------------
+DROP TABLE IF EXISTS `auctionList`;
+CREATE TABLE `auctionList` (
+  `sellerId` int unsigned NOT NULL,
+  `proId` int unsigned NOT NULL,
+  `postDate` datetime NOT NULL,
+  PRIMARY KEY (`sellerId`, `proId`),
+  CONSTRAINT `FK_auctionList_users` FOREIGN KEY (`sellerId`) REFERENCES `users` (`userId`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `FK_auctionList_products` FOREIGN KEY (`proId`) REFERENCES `products` (`proId`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- ----------------------------
+-- Table structure for upgrade list
+-- ----------------------------
+DROP TABLE IF EXISTS `upgradeList`;
+CREATE TABLE `upgradeList` (
+  `bidderId` int unsigned NOT NULL,
+  `registerTime` datetime NOT NULL,
+  PRIMARY KEY (`bidderId`),
+  CONSTRAINT `FK_upgradeList_users` FOREIGN KEY (`bidderId`) REFERENCES `users` (`userId`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
