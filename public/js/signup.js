c27dd1632f468437c6baac8042c9e2de514ac420
Phineasla <63248715+Phineasla@users.noreply.github.com>
2021-12-08 19:35:57 +0700

    Add redirect to signup

--- public/js/signup.js
+++ public/js/signup.js
@@ -3,14 +3,16 @@ document.getElementsByTagName('form')[0].onsubmit = async function (e) {
   const formData = new FormData(e.target);
   const res = await fetch(e.target.getAttribute('action'), {
     method: e.target.getAttribute('method'),
-    body: new URLSearchParams(formData),
+    redirect: 'follow',
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded',
     },
+    body: new URLSearchParams(formData),
   });
-  if(res.ok) {
-    
+  if (res.redirected) {
+    window.location.href = res.url;
+  } else {
+    const resJson = await res.json();
+    resJson.forEach((err) => toastr.error(err.msg));
   }
-  const resJson = await res.json();
-  resJson.forEach((err) => toastr.error(err.msg));
 };
