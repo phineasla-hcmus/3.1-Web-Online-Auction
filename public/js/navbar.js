fe21a0662eaee32d3084e1f03400fd5ab4b9be9b
nntt1210 <58521730+nntt1210@users.noreply.github.com>
2021-12-08 21:56:04 +0700

    add responsive navbar with mobile menu

---
+++ public/js/navbar.js
@@ -0,0 +1,46 @@
+const btnHamburger = document.querySelector('#btnHamburger');
+const body = document.querySelector('body');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+const fadeElems = document.querySelectorAll('.has-fade');
+
+btnHamburger.addEventListener('click', function () {
+  console.log('open hamburger');
+  // Close hamburger menu
+  if (header.classList.contains('open')) {
+    body.classList.remove('noscroll');
+    header.classList.remove('open');
+    fadeElems.forEach(function (element) {
+      element.classList.remove('fade-in');
+      element.classList.add('fade-out');
+    });
+  }
+  // Open hamburger menu
+  else {
+    body.classList.add('noscroll');
+    header.classList.add('open');
+    fadeElems.forEach(function (element) {
+      element.classList.remove('fade-out');
+      element.classList.add('fade-in');
+    });
+  }
+});
+
+const menu_item = document.querySelectorAll('.header__menu--item > a');
+const menu_dropdown = document.querySelectorAll('.header__dropdown');
+menu_item.forEach(function (element, index) {
+  console.log(element.childNodes);
+  element.addEventListener('click', function () {
+    console.log('item clicked');
+    let item = menu_dropdown[index];
+    if (item.classList.contains('open')) {
+      item.classList.remove('open');
+      //   item.classList.remove("fade-in");
+      //   item.classList.add("fade-out");
+    } else {
+      item.classList.add('open');
+      //   item.classList.remove("fade-out");
+      //   item.classList.add("fade-in");
+    }
+  });
+});
