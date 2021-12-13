3f2e630563d4a1866174168faf10f86cccec59a5
nntt1210 <58521730+nntt1210@users.noreply.github.com>
2021-12-13 21:32:54 +0700

    update product model and render different menu when logged in

--- app/models/product.model.ts
+++ app/models/product.model.ts
@@ -1,13 +1,25 @@
 import db from '../config/database';
 export default {
   async findNearEndProducts() {
-    return db('products').orderBy('expiredDate', 'asc').limit(5);
+    let date = new Date();
+    return db('products')
+      .where('expiredDate', '>=', date)
+      .orderBy('expiredDate', 'asc')
+      .limit(5);
   },
   async findMostBidsProducts() {
-    return db('products').orderBy('numberOfBids', 'desc').limit(5);
+    let date = new Date();
+    return db('products')
+      .where('expiredDate', '>=', date)
+      .orderBy('numberOfBids', 'desc')
+      .limit(5);
   },
   async findHighestPriceProducts() {
-    return db('products').orderBy('currentPrice', 'desc').limit(5);
+    let date = new Date();
+    return db('products')
+      .where('expiredDate', '>=', date)
+      .orderBy('currentPrice', 'desc')
+      .limit(5);
   },
   async findProductbyCategory(catid: string | number | Readonly<any> | null) {
     return db('products')
