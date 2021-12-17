4deaee002fbd91c24d058b5a2a2acd2ef8e77503
nntt1210 <58521730+nntt1210@users.noreply.github.com>
2021-12-17 16:47:51 +0700

    add dashboard for bidder

--- app/routes/bidder.ts
+++ app/routes/bidder.ts
@@ -5,31 +5,35 @@ import productModel from '../models/product.model';
 const bidderRouter = Router();
 
 bidderRouter.get('/info', function (req, res) {
-  res.render('bidder/info');
+  res.render('bidder/info', {
+    layout: 'bidder',
+  });
 });
 
 bidderRouter.get('/favorite', async function (req, res) {
   // 1 để render tạm thời, có login sửa sau
   const favoriteList = await bidderModel.getFavoriteList(1);
-  res.render('bidder/favorite', { favoriteList });
+  res.render('bidder/favorite', { layout: 'bidder', favoriteList });
 });
 
 bidderRouter.get('/currentbids', async function (req, res) {
   // 1 để render tạm thời, có login sửa sau
   const currentBidsList = await bidderModel.getCurrentBids(1);
   console.log(currentBidsList);
-  res.render('bidder/currentBid', { currentBidsList });
+  res.render('bidder/currentBid', { layout: 'bidder', currentBidsList });
 });
 
 bidderRouter.get('/rating', async function (req, res) {
   // 1 để render tạm thời, có login sửa sau
-  res.render('bidder/rating');
+  res.render('bidder/rating', {
+    layout: 'bidder',
+  });
 });
 
 bidderRouter.get('/win', async function (req, res) {
   // 1 để render tạm thời, có login sửa sau
   const winningList = await bidderModel.getWinningList(1);
-  res.render('bidder/win', { winningList });
+  res.render('bidder/win', { layout: 'bidder', winningList });
 });
 
 export default bidderRouter;
