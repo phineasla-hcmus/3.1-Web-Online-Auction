4795a8a5ebb551333ae8a3911e9613070f2eb612
19127203 <64020154+19127203@users.noreply.github.com>
2021-12-17 19:25:19 +0700

    change nhẹe

--- app/models/category.model.ts
+++ app/models/category.model.ts
@@ -2,6 +2,7 @@ import db from "../config/database";
 export default {
   findParentCategory() {
     return db("categories").where("parentId", null);
+    
   },
   async findChildCategory() {
     return db("categories").where("parentId", ">=", 1);
