1660276ebc13bf5eee397b5950cd0426df269d4e
Phineasla <63248715+Phineasla@users.noreply.github.com>
2022-01-05 22:47:42 +0700

    Refactor

--- app/routes/seller.ts
+++ app/routes/seller.ts
@@ -4,25 +4,23 @@ import sellerModel from '../models/seller.model';
 const sellerRouter = Router();
 
 //TODO PhineasLa
-sellerRouter.get('/viewPostedProduct', async function (req,res){
-    res.render('seller/viewPostedProduct', {
-        layout: 'bidder',
-        postedProduct: true,
-      });
+sellerRouter.get('/my-product', async function (req, res) {
+  res.render('seller/myProduct', {
+    layout: 'bidder',
+    postedProduct: true,
+  });
 });
 
 //TODO PhineasLa
-sellerRouter.get('/uploadProduct', async function (req,res){
-  res.render('seller/uploadProduct', {
-      layout: 'bidder',
-      uploadProduct: true,
-    });
+sellerRouter.get('/add-product', async function (req, res) {
+  res.render('seller/addProduct', {
+    layout: 'bidder',
+    uploadProduct: true,
+  });
 });
 
-sellerRouter.post('/denyBidder',async function(req,res){
+sellerRouter.post('/denyBidder', async function (req, res) {
   const proId = req.body.proId;
-  const bidderId=req.body.bidderId;
-
-  
-})
+  const bidderId = req.body.bidderId;
+});
 export default sellerRouter;
