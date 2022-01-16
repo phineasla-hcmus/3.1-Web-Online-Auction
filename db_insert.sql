7d903c3e930990324917810ee97f7cb5e11fdea2
Phineasla <63248715+Phineasla@users.noreply.github.com>
2022-01-16 10:01:03 +0700

    Endgame

--- db_insert.sql
+++ db_insert.sql
@@ -38,8 +38,6 @@ VALUES (13, 'Jewelry', NULL);
 INSERT INTO categories
 VALUES (14, 'Watches', 4);
 
-INSERT INTO users VALUES (1,'nguyenngocthanhtam9b@gmail.com','$2a$10$knCAXg.TOhk69XK246nUnOhSNvM3fR5PjVWVR1cY5y6VecpatGh8G','Tam','Nguyen','2001-10-12','somewhere in the universe',8,2,0);
-INSERT INTO users VALUES (2,'popcorn.software.engineering@gmail.com','$2a$10$knCAXg.TOhk69XK246nUnOhSNvM3fR5PjVWVR1cY5y6VecpatGh8G','Hong','Du Shik','1995-10-12','somewhere',5,3,0);
 INSERT INTO users VALUES (3,'leesuby.tl@gmail.com','$2a$10$knCAXg.TOhk69XK246nUnOhSNvM3fR5PjVWVR1cY5y6VecpatGh8G','Long','Bi','2001-10-12','somewhere in the universe',10,3,0);
 INSERT INTO users VALUES (4,'admin@gmail.com','$2a$10$knCAXg.TOhk69XK246nUnOhSNvM3fR5PjVWVR1cY5y6VecpatGh8G','Long','Long','2001-10-12','somewhere in the universe',NULL,4,0);
 -- sample data used for [admin] manage usersINSERT INTO users
